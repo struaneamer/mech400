2c09e94b411011e572a4f26f1322322c9d4b2705
Struan Eamer <struaneamer@gmail.com>
2020-07-20 22:40:32 -0700

    Update teammongoose.ino

--- teammongoose.ino
+++ teammongoose.ino
@@ -39,10 +39,6 @@ int Kd = 0.03;
 
 PID balancePID(&Input,&Output,&Setpoint,Kp,Ki,Kd,DIRECT);  
 
-    //int neutral = 1300;
-    //int fullForward = 2600;
-    //int fullReverse = 0;
-
     //End PID Balance//
 
     //Start Angle integration//
@@ -52,7 +48,7 @@ int16_t angle[2]; // pitch & roll
 FreeSixIMU sixDOF = FreeSixIMU();
 int rawSixDof[6]; 
 int16_t _atan2(int32_t y, int32_t x);
-int getAngle();
+float getAngle();
 
     //End Angle Integration//
 
@@ -79,33 +75,8 @@ void setup()
   balancePID.SetMode(AUTOMATIC);
   balancePID.SetOutputLimits(-255,255);
   Serial.begin(9600);         
-                                // initialize all the readings to 0:
-  /*
-  Serial.println("Calibration procedure for Mamba ESC.");
-  Serial.println("Turn on ESC.");
-  Serial.println("Press any key to continue.");
-  while (!Serial.available());
-  Serial.read();
+      // initialize all the readings to 0:
   
-  firstESC.writeMicroseconds(0);
-  Serial.println("Starting Calibration.");
-  delay(5000);
-  firstESC.writeMicroseconds(2600);
-  Serial.println("Writing Full Throttle.");
-  delay(5000);
-  firstESC.writeMicroseconds(0);
-  Serial.println("Writing Full Reverse.");
-  delay(5000);
-  firstESC.writeMicroseconds(1300);
-  Serial.println("Writing Neutral.");
-  delay(10000);
-  Serial.println("Calibration Complete.");
-
-
-  for (int thisReading = 0; thisReading < numReadings; thisReading++){ 
-    readings[thisReading] = 0;
-  }
-  */ 
       //End PID Balance//
 }
 
@@ -113,13 +84,13 @@ void loop(){
  
   Input = getAngle();                                               //Removed Smoothing, likely done by complementary filter in "getAngle()"
     
-    //Serial.println(" ");
+    //Serial.println(" ");    //Debugging
     //Serial.print("X:"); 
     //Serial.print(Input);
    
     balancePID.Compute();                                                  //Supposed to pass this a "double pos", unsure how this works, possibly for Driven Wheel Balance. Calculates "output = Kp * error + integral- Kd * dInput;"
     
-    //Serial.print(" ");
+    //Serial.print(" ");      //Debugging
     //Serial.print("PIDOutput:");
     //Serial.print(Output);
     //Serial.println("");
@@ -128,55 +99,7 @@ void loop(){
 
 }
 
-//int motorSpeed(int newValue){
-//    motor.setSpeed(newValue);
-//}
 /*
-int averageValue(int GyX){
-    total= total - readings[index];
-    readings[index] = GyX;
-    total= total + readings[index];     
-    index = index + 1;                    
-    if (index >= numReadings){              
-      index = 0;                          
-    }
-    average = total / numReadings;  
-    //Serial.print("Average Value (X): ");Serial.println(average); 
-   return average;   
-}
-
-int readGyro(){
-  Wire.beginTransmission(MPU);
-  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
-  Wire.endTransmission(false);
-  Wire.requestFrom(MPU,14,true);  // request a total of 14 registers
-  
-  AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)     
-  AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
-  AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
-  Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
-  GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
-  GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
-  GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
-  
-//  Serial.print("AcX = "); Serial.print(AcX);
-//  Serial.print(" | AcY = "); Serial.print(AcY);
-//  Serial.print(" | AcZ = "); Serial.print(AcZ);
-//  Serial.print(" | Tmp = "); Serial.print(Tmp/340.00+36.53);  //equation for temperature in degrees C from datasheet
-//  Serial.println(" | GyX = "); Serial.print(GyX);
-//  Serial.print(" | GyY = "); Serial.print(GyY);
-//  Serial.print(" | GyZ = "); Serial.println(GyZ);
-  
-//delay(333);
-  if(AcY < 0){
-    return ((AcX^2)+(AcY^2))^(1/2);
-  }
-  if(AcY > 0){
-    return -((AcX^2)+(AcY^2))^(1/2);
-  }
-}
-
-
 int smooth(int data, float filterVal, float smoothedVal){
 
 
@@ -193,11 +116,10 @@ int smooth(int data, float filterVal, float smoothedVal){
 }
 */
 
-int getAngle() 
+float getAngle() 
 {
-  int angleTemp = 0;
+  float angleTemp = 0;
      sixDOF.getRawValues(rawSixDof);
- //
   
   angle[0] = _atan2(rawSixDof[0],rawSixDof[2]);
   //angle[1] = _atan2(rawSixDof[1],rawSixDof[2]);
